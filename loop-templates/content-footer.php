1eae140e62b7423684f6799346aa9b5a92176bf8
Ahmed Khelifi <contact@ahmedkhelifi.com>
2017-08-22 14:13:07 +0200

    footer changes

--- loop-templates/content-footer.php
+++ loop-templates/content-footer.php
@@ -8,71 +8,79 @@
 
 $prefix = 'fl-footer-';
 
-$images = rwmb_meta( $prefix.'image', 'size=full' ); // Since 4.8.0
-if ( !empty( $images ) ) {
-    foreach ( $images as $image ) {
-        $image = $image;
-    }
-
-}
-
 ?>
 <div class="section" id="section10" style="background-image:url('<?php echo $image['url'] ?>');">
-<div class="container-fluid">
-    <?php
-    $images = rwmb_meta( $prefix.'image', 'size=full' ); // Since 4.8.0
+    <div class="container-fluid">
 
 
-    if ( !empty( $images ) ) {
+        <?php
+        $images = rwmb_meta( $prefix.'image', 'size=full' ); // Since 4.8.0
 
-        $image = $images[0]['url'];
+        if (!empty($images)) {
+            foreach ($images as $image) {
+                $image = $image;
+                //echo '<img src="', esc_url($image['url']), '"  alt="', esc_attr($image['alt']), '">';
+            }
+        }
 
-    }
-    ?>
-    <div class="row">
+        ?>
+      <?php /*  <div class="col-md-6 image" style="background-image:url('<?php echo $image['url'] ?>');">
+
+            <?php
+            $images = rwmb_meta( $prefix.'image', 'size=full' ); // Since 4.8.0
+
+
+            if ( !empty( $images ) ) {
+
+                $image = $images[0]['url'];
+
+            }
+            ?>
+        </div> */ ?>
 
 
         <div class="col-md-6">
+            <div class="row">
 
-        <ul class="nav">
-            <li class="nav-item">
-                <a class="nav-link active" href="#">Active</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link disabled" href="#">Disabled</a>
-            </li>
-        </ul>
+
+                <div class="col-md-12">
+
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <h2 class="nav-link active" href="#">KONTAKT :</h2>
+                        </li>
+
+                        <p>.Mail: info@flustix.com <br>·Phone: +49 (0) 30 80 93 331-11 <br> Kronprinzendamm 20, 10711 Berlin</p>
+
+                        <li class="nav-item">
+                            <h2 class="nav-link" href="#">Newsletter</h2>
+                        </li>
+                        <li class="nav-item">
+                            <h2 class="nav-link" href="#">Videos</h2>
+                        </li>
+                        <li class="nav-item">
+                            <h2 class="nav-link" href="#">FAQ's</h2>
+                        </li>
+                        <li class="nav-item">
+                            <h2 class="nav-link" href="#">Rechtliche Hinweise</h2>
+                        </li>
+                        <li class="nav-item">
+                            <h2 class="nav-link" href="#">Datenschutz</h2>
+                        </li>
+                    </ul>
+
+                </div>
+
+                <div class="col-md-12">
+                    <ul class="nav">
+                        <li class="nav-item">
+                            <a style="height:90%; ">&copy; FLUSTIX GmbH 2017</a>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
 
         </div>
-
-        <div class="col-md-4 col-md-offset-4">
-            <ul class="nav">
-                <li class="nav-item">
-                    <a class="nav-link active" href="#">info@flustix.com</a><a href="tel:+49(0)308093331-11">+49 (0) 30 80 93 331-11</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Kronprinzendamm 20, 10711 Berlin</a>
-                </li>
-            </ul>
-
-        </div>
-
-        <div class="col-md-4">
-            <ul class="nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">&copy; FLUSTIX GmbH 2017</a>
-                </li>
-
-            </ul>
-        </div>
-
-
     </div>
-</div>
 </div>
